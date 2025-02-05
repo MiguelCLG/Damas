beadef07c966d6d2b8463ca484e4e230d5eaba50
Miguel Gonçalves <miguelclg@hotmail.com>
2025-02-05 23:02:05 +0000

    Main Menu added
    - Main menu now has updated visuals

--- Scripts/Utils.cs
+++ Scripts/Utils.cs
@@ -2,7 +2,7 @@
 public static class Utils
 {
   public const int ViewportBaseX = 600;
-  public const int ViewportBaseY = 1024;
+  public const int ViewportBaseY = 800;
   public const int BoardSize = 8;
   public const int CheckerSize = 64;
   public const int TileSize = 64;
