ad5b787182abd53b3d31d1a574faf8280a8bcbcf
Miguel Gonçalves <miguelclg@hotmail.com>
2025-02-10 21:03:05 +0000

    release v0.2

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1738968832|4102810936";
+const CACHE_VERSION = "1739221343|5218579369";
 const CACHE_PREFIX = "Damas-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
