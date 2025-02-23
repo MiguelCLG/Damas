d193592c3bb1c8885cc7e99e50194a0823ab7631
Lavizord <63145101+Lavizord@users.noreply.github.com>
2025-02-23 13:10:05 +0000

    feat: implemented web utils, and server params for connectiona re taken from the url params of godot.

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -17,98 +17,98 @@ public partial class MainMenu : Control
 
   public override void _Ready()
   {
-    ViewportBaseX = GetViewportRect().Size.x;
-    ViewportBaseY = GetViewportRect().Size.y;
-    sceneLoaders = GetNode<SceneLoaders>("/root/SceneLoaders");
-    roomPopup = GetNode<RoomPopup>("%RoomPopup");
-    //  roomList = GetNode<Panel>("%RoomList");
-    PlayerName = GetNode<Label>("%PlayerName");
-    PlayerMoney = GetNode<Label>("%PlayerMoney");
-    WaitingQueueLabel = GetNode<RichTextLabel>("%WaitingQueueLabel");
-    BidTexture = GetNode<TextureRect>("%BidTexture");
+	ViewportBaseX = GetViewportRect().Size.x;
+	ViewportBaseY = GetViewportRect().Size.y;
+	sceneLoaders = GetNode<SceneLoaders>("/root/SceneLoaders");
+	roomPopup = GetNode<RoomPopup>("%RoomPopup");
+	//  roomList = GetNode<Panel>("%RoomList");
+	PlayerName = GetNode<Label>("%PlayerName");
+	PlayerMoney = GetNode<Label>("%PlayerMoney");
+	WaitingQueueLabel = GetNode<RichTextLabel>("%WaitingQueueLabel");
+	BidTexture = GetNode<TextureRect>("%BidTexture");
 
-    EventSubscriber.SubscribeToEvent("OnGameStarting", OnGameStarting);
+	EventSubscriber.SubscribeToEvent("OnGameStarting", OnGameStarting);
   }
 
   private void OnGameStarting(object sender, object args)
   {
-    if (args is GameStartMessage message)
-    {
-      initialBoard = message.Board;
-      foreach (DictionaryEntry p in initialBoard)
-      {
-        GD.Print(p.Key);
+	if (args is GameStartMessage message)
+	{
+	  initialBoard = message.Board;
+	  foreach (DictionaryEntry p in initialBoard)
+	  {
+		GD.Print(p.Key);
 
-      }
-      blackPieceDirection = currentGameColor == BoardColors.Black ? -1 : 1;
-      foreach (var gamePlayer in message.GamePlayers)
-      {
-        GD.Print($"Message: {gamePlayer.name}, State: {playerName}");
-        if (playerName == gamePlayer.name)
-        {
-          player = gamePlayer;
-        }
-      }
-    }
-    LoadNextScene();
+	  }
+	  blackPieceDirection = currentGameColor == BoardColors.Black ? -1 : 1;
+	  foreach (var gamePlayer in message.GamePlayers)
+	  {
+		GD.Print($"Message: {gamePlayer.name}, State: {playerName}");
+		if (playerName == gamePlayer.name)
+		{
+		  player = gamePlayer;
+		}
+	  }
+	}
+	LoadNextScene();
 
   }
 
   private void LoadNextScene()
   {
-    sceneLoaders.NextScene();
+	sceneLoaders.NextScene();
   }
 
   public void OnArrowClicked(int direction)
   { // 0 - left; 1 - right
-    if (direction == 0)
-    {
-      var currentTexture = BidTexture.Texture;
-      var index = bidTextures.IndexOf(currentTexture);
-      if (index == 0) index = bidTextures.Count;
-      BidTexture.Texture = bidTextures[index - 1];
-      betValue = availableBidValues[index - 1];
-    }
-    else
-    {
-      var currentTexture = BidTexture.Texture;
-      var index = bidTextures.IndexOf(currentTexture);
-      if (index == bidTextures.Count - 1) index = -1;
-      BidTexture.Texture = bidTextures[index + 1];
-      betValue = availableBidValues[index + 1];
-    }
+	if (direction == 0)
+	{
+	  var currentTexture = BidTexture.Texture;
+	  var index = bidTextures.IndexOf(currentTexture);
+	  if (index == 0) index = bidTextures.Count;
+	  BidTexture.Texture = bidTextures[index - 1];
+	  betValue = availableBidValues[index - 1];
+	}
+	else
+	{
+	  var currentTexture = BidTexture.Texture;
+	  var index = bidTextures.IndexOf(currentTexture);
+	  if (index == bidTextures.Count - 1) index = -1;
+	  BidTexture.Texture = bidTextures[index + 1];
+	  betValue = availableBidValues[index + 1];
+	}
   }
   public void SetPlayerName(string newPlayerName)
   {
-    PlayerName.Text = newPlayerName;
+	PlayerName.Text = newPlayerName;
   }
   public void SetPlayerMoney(string newPlayerMoney)
   {
-    PlayerMoney.Text = newPlayerMoney;
+	PlayerMoney.Text = newPlayerMoney;
   }
 
   public void SetWaitingQueue(string numberOfPlayers)
   {
-    WaitingQueueLabel.Text = $"{numberOfPlayers} jogadores na fila.";
+	WaitingQueueLabel.Text = $"{numberOfPlayers} jogadores na fila.";
   }
   public void ShowRoom()
   {
-    roomPopup.SetBidTexture(BidTexture.Texture);
-    roomPopup.Visible = true;
+	roomPopup.SetBidTexture(BidTexture.Texture);
+	roomPopup.Visible = true;
   }
   public void HideRoom()
   {
-    roomPopup.Visible = false;
+	roomPopup.Visible = false;
   }
 
   public void OnStartGame()
   {
-    EventRegistry.GetEventPublisher("OnJoinRoom").RaiseEvent(betValue);
+	EventRegistry.GetEventPublisher("OnJoinRoom").RaiseEvent(betValue);
   }
 
   public override void _ExitTree()
   {
-    EventSubscriber.UnsubscribeFromEvent("OnGameStarting", OnGameStarting);
+	EventSubscriber.UnsubscribeFromEvent("OnGameStarting", OnGameStarting);
   }
 }
 
@@ -118,11 +118,11 @@ public partial class MainMenu : Control
 
 /* public void ShowRoomList()
   {
-    roomList.Visible = true;
+	roomList.Visible = true;
   }
   public void HideRoomList()
   {
-    roomList.Visible = false;
+	roomList.Visible = false;
   } */
 /* public void OnSalasClick()
 {
---
+++ Scripts/WebUtils/WebUtils.cs
@@ -0,0 +1,90 @@
+using Godot;
+using System.Collections.Generic;
+
+
+public class UrlParamsModel
+{
+	public string Token { get; set; }
+	public string SessionId { get; set; }
+	public string Currency { get; set; }
+
+	public UrlParamsModel(string token, string sessionId, string currency)
+	{
+		Token = token;
+		SessionId = sessionId;
+		Currency = currency;
+	}
+}
+
+// Here are some examples of URL params to add to our game url.
+
+// ?token=abc123&sessionid=xyz456&currency=USD
+// ?token=token456&sessionid=session2&currency=USD
+public static class WebUtils
+{
+	public static Dictionary<string, string> GetUrlParams()
+	{
+		string osName = OS.GetName();
+
+		if (osName != "Web" && osName != "HTML5") {
+			GD.Print($"[ URL PARAMS ] :: Running on non-HTML environment: {OS.GetName()}");
+			return new Dictionary<string, string>();
+		}
+
+		string script = @"
+			var params = new URLSearchParams(window.location.search);
+			var obj = {};
+			params.forEach((value, key) => { obj[key] = value; });
+			console.log('Parsed URL Params:', obj); // Debug line
+			JSON.stringify(obj); // No return statement
+		";
+
+		string result = (string)JavaScript.Eval(script);
+		
+		// Debug print to see the raw JSON result
+		GD.Print("[ URL PARAMS ] :: Raw JSON Result: ", result);
+		
+		var jsonParseResult = JSON.Parse(result);
+		if (jsonParseResult.Error == Error.Ok && jsonParseResult.Result is Godot.Collections.Dictionary dict)
+		{
+			var parameters = new Dictionary<string, string>();
+			foreach (var key in dict.Keys)
+			{
+				parameters[key.ToString()] = dict[key].ToString();
+			}
+			return parameters;
+		}
+
+		return new Dictionary<string, string>();
+	}
+
+	public static UrlParamsModel GetUrlParamsModel()
+	{
+		var parameters = GetUrlParams();
+		// Extract values safely
+		string token = parameters.ContainsKey("token") ? parameters["token"] : "N/A";
+		string sessionId = parameters.ContainsKey("sessionid") ? parameters["sessionid"] : "N/A";
+		string currency = parameters.ContainsKey("currency") ? parameters["currency"] : "N/A";
+
+		return new UrlParamsModel(token, sessionId, currency);
+	}
+
+
+	public static void PrintUrlParams()
+	{
+		var parameters = GetUrlParams();
+		
+		// Extract values safely
+		string token = parameters.ContainsKey("token") ? parameters["token"] : "N/A";
+		string sessionId = parameters.ContainsKey("sessionid") ? parameters["sessionid"] : "N/A";
+		string currency = parameters.ContainsKey("currency") ? parameters["currency"] : "N/A";
+
+		// Print to Godot output
+		GD.Print($"[ URL PARAMS ] :: Token: {token}, Session ID: {sessionId}, Currency: {currency}");
+
+		// Print to browser console in a clean way
+		string script = $"console.log('[ URL PARAMS ] :: Token:', '{token}', 'Session ID:', '{sessionId}', 'Currency:', '{currency}');";
+		JavaScript.Eval(script);
+	}
+
+}
