a33ffb5795a184771805c55d73ba24b493586e26
Miguel Gonçalves <miguelclg@hotmail.com>
2025-03-15 15:50:45 +0000

    release: v0.8: options menu and translations

--- docs/docs.service.worker.js
+++ docs/docs.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1741439014|5978388821";
+const CACHE_VERSION = "1742509123|11699461317";
 const CACHE_PREFIX = "Damas-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "docs.offline.html";
